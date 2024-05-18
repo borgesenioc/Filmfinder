d79dc398d0c60d47f2324ae49950526a5e13c2c3
borgesenioc <borgesenioc@gmail.com>
2024-05-18 10:48:45 -0300

    Completed steps 1 to 12 from the Filmfinder project

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-const tmdbKey = '';
-const tmdbBaseUrl = '';
+const tmdbKey = '861323a96197f76151c253f6c744a6c0';
+const tmdbBaseUrl = 'https://api.themoviedb.org/3';
 const playBtn = document.getElementById('playBtn');
 
-const getGenres = () => {
+const getGenres = async () => {
+  const genreRequestEndpoint = 'genre/movie/list';
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
 
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+        const jsonResponse = await response.json();
+        console.log(jsonResponse);
+        const genres = jsonResponse.genres;
+        console.log(genres)
+    }
+ 
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const getMovies = () => {
