17c130e837e9ceb6d7cdc20dec0dc9c960765e4b
borgesenioc <borgesenioc@gmail.com>
2024-05-20 08:55:07 -0300

    Finished the Filmfinder exercise by adding logic to the getMovieInfo and ShowRandomMovie functions of the Script

--- script.js
+++ script.js
@@ -41,17 +41,34 @@ const getMovies = async () => {
 };
 
 
-const getMovieInfo = () => {
- 
+const getMovieInfo = async (movie) => {
+  const movieId = movie.id;
+  const movieEndpoint = `movie/${movieId}`;
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${movieEndpoint}${requestParams}`;
+
+  try {
+   const response = await fetch(urlToFetch);
+   if (response.ok) {
+     const movieInfo = await response.json();
+     return movieInfo;
+   }
+
+  } catch(error) {
+   console.log(error);
+  }
 };
 
 // Gets a list of movies and ultimately displays the info of a random movie from the list
-const showRandomMovie = () => {
+const showRandomMovie = async () => {
   const movieInfo = document.getElementById('movieInfo');
   if (movieInfo.childNodes.length > 0) {
     clearCurrentMovie();
   };
-
+  const movies = await getMovies();
+  const randomMovie = getRandomMovie(movies);
+  const info = await getMovieInfo(randomMovie);
+  displayMovie(info);
 };
 
 getGenres().then(populateGenreDropdown);
